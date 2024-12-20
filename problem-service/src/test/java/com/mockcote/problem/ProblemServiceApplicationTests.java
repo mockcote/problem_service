23dc9864cfe23448444bdd8592b5362d929df912
dajeong11 <122199259+dajeong11@users.noreply.github.com>
2024-12-20 12:58:21 +0900

    init project

---
+++ problem-service/src/test/java/com/mockcote/problem/ProblemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockcote.problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
