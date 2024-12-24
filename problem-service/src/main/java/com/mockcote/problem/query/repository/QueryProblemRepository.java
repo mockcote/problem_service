c2c28103c25d7a9a80998879938941fa7e3237e3
dajeong11 <122199259+dajeong11@users.noreply.github.com>
2024-12-24 19:36:46 +0900

    backup

--- problem-service/src/main/java/com/mockcote/problem/query/repository/QueryProblemRepository.java
+++ problem-service/src/main/java/com/mockcote/problem/query/repository/QueryProblemRepository.java
@@ -2,7 +2,6 @@ package com.mockcote.problem.query.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.mockcote.problem.query.entity.QueryProblem;
 
