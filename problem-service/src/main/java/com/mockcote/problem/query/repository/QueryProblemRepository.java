44472e6e3b0eba311f1fcc8fa3233658d57a96de
dajeong11 <122199259+dajeong11@users.noreply.github.com>
2024-12-24 18:29:32 +0900

    Fix: 사용자 지정 쿼리 버그 수정

--- problem-service/src/main/java/com/mockcote/problem/query/repository/QueryProblemRepository.java
+++ problem-service/src/main/java/com/mockcote/problem/query/repository/QueryProblemRepository.java
@@ -2,6 +2,7 @@ package com.mockcote.problem.query.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mockcote.problem.query.entity.QueryProblem;
 
@@ -14,39 +15,38 @@ public interface QueryProblemRepository extends JpaRepository<QueryProblem, Inte
      * userId가 이미 푼 문제는 제외하고 조건을 만족하는 문제 1개를 찾는다.
      */
 	@Query(value = """
-	        SELECT p.*
-	          FROM problem p
-	          LEFT JOIN user_solved_problem usp
-	                 ON p.problem_id = usp.problem_id
-	                AND usp.user_id = ?1
-	         WHERE usp.problem_id IS NULL
-	           AND p.difficulty BETWEEN ?2 AND ?3
-	           AND p.acceptable_user_count BETWEEN ?4 AND ?5
-	           AND (?6 IS NULL OR EXISTS (
-	               SELECT 1
-	               FROM problem_tag pt
-	               WHERE pt.problem_id = p.problem_id
-	                 AND pt.tag_id IN (?6)
-	           ))
-	           AND NOT EXISTS (
-	               SELECT 1
-	               FROM problem_tag pt
-	               WHERE pt.problem_id = p.problem_id
-	                 AND pt.tag_id IN (?7)
-	           )
-	         LIMIT ?8
-	        """, nativeQuery = true)
-	List<QueryProblem> findCandidateProblemsExcludingSolved(
-	    String userId,
-	    int minDifficulty,
-	    int maxDifficulty,
-	    int minAcceptableUserCount,
-	    int maxAcceptableUserCount,
-	    List<Integer> desiredTags,
-	    List<Integer> undesiredTags,
-	    int limitSize
-	);
-
+		    SELECT p.*
+		      FROM problem p
+		      LEFT JOIN user_solved_problem usp
+		             ON p.problem_id = usp.problem_id
+		            AND usp.user_id = ?1
+		     WHERE usp.problem_id IS NULL
+		       AND p.difficulty BETWEEN ?2 AND ?3
+		       AND p.acceptable_user_count BETWEEN ?4 AND ?5
+		       AND EXISTS (
+		           SELECT 1
+		           FROM problem_tag pt
+		           WHERE pt.problem_id = p.problem_id
+		             AND pt.tag_id IN (?6)
+		       )
+		       AND NOT EXISTS (
+		           SELECT 1
+		           FROM problem_tag pt
+		           WHERE pt.problem_id = p.problem_id
+		             AND pt.tag_id IN (?7)
+		       )
+		     LIMIT ?8
+		    """, nativeQuery = true)
+		List<QueryProblem> findCandidateProblemsExcludingSolved(
+		    String userId,
+		    int minDifficulty,
+		    int maxDifficulty,
+		    int minAcceptableUserCount,
+		    int maxAcceptableUserCount,
+		    List<Integer> desiredTags,
+		    List<Integer> undesiredTags,
+		    int limitSize
+		);
 	
 	@Query(value = """
 	        SELECT * 
