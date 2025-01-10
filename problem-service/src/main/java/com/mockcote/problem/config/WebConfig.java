26adb9279c73597a0cc2d2524e98b77d3cfd39bf
dajeong11 <dajeongkim.mail@gmail.com>
2025-01-10 10:26:55 +0900

    Fix: 프론트 도메인 CORS 추가

--- problem-service/src/main/java/com/mockcote/problem/config/WebConfig.java
+++ problem-service/src/main/java/com/mockcote/problem/config/WebConfig.java
@@ -20,13 +20,18 @@ public class WebConfig implements WebMvcConfigurer {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-    
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 설정 적용
-                .allowedOrigins("http://localhost:3000") // 허용할 클라이언트 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 쿠키 허용
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://mockcote.site",
+                        "https://mockcote.site" // 프론트엔드 배포 도메인 추가
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+
 }
